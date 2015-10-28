bdf194db51a409c30416d9d7ca7685139d81a6fa
xugang <xugang0409@163.com>
2015-10-28 20:52:04 +0800

    我的白条模块

--- projects/purchase/baitiao/index.js
+++ projects/purchase/baitiao/index.js
@@ -1,12 +1,6 @@
 //引入已经激活的首页的样式表
+require('../_stylesheets/baitiao.less');
 
-var { UI } = require('../../../shared/jquery/components/core');
-
-/**
- * 我的白条详情首页： 账单分类查询，。。。。
- * 入口启动代码放到UI.ready();
- *
- */
-UI.ready(function () {
-
-});
+require('../../../shared/jquery/components/timeline');
+require('../../../shared/jquery/components/tabs');
+require("../../../shared/jquery/components/pagination");
