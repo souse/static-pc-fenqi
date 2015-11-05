11785d1466c6524e01dd50d598f95ab00f1a7ff2
xugang <xugang0409@163.com>
2015-11-05 18:36:48 +0800

    update purchase,home

--- projects/home/introduction/index.js
+++ projects/home/introduction/index.js
@@ -1,2 +1,49 @@
 //引入已经激活的首页的样式表,此页面应该只有 样式不需要JS
-require('../_stylesheets/index.less');
+require('../_stylesheets/introduction.less');
+require('../../../shared/jquery/components/popup');
+var {
+  UI
+} = require('../../../shared/jquery/components/core');
+
+
+UI.ready(function() {
+  var otpInstance = $('.alert-open').getInstance();
+  console.log(optInstance);
+  otpInstance.setOptions({
+    onOpen: false,
+    onClose: false,
+    onActionClicked: false,
+    autoClose: false,
+    modal: true,
+    modalClose: false,
+    classes: "",
+    header: {
+      showClose: true,
+      html: "Your Header"
+    },
+    body: "Your dialog body",
+    footer: {
+      html: '<button class="btn btn-primary btn-sm btn-popup" data-trigger="ok">确定</button>'
+    }
+  });
+
+
+  $("#button").on('click', '.alert-open', function() {
+    dialog.alert({
+      onOpen: function() {
+        $result.find('.output').html('on open..');
+      },
+      onClose: function() {
+        $result.find('.output').html('on close..');
+      },
+      onActionClicked: function($btn) {
+        $result.find('.output').html($btn.data('trigger'));
+        // clicked close dialog.
+        this.close();
+      }
+    });
+  });
+
+
+
+});
