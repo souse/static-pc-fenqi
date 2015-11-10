e2e7e0a28ac39f3e27b900d4445c7a155410d51b
xugang <xugang0409@163.com>
2015-11-10 16:17:40 +0800

    update payment

--- projects/payment/payment_result/index.js
+++ projects/payment/payment_result/index.js
@@ -1,21 +1,14 @@
 //引入样式表
 require("../_stylesheets/payment_result.less");
-require("../../../shared/widgets/_stylesheets/module.spread.result.less");
+require("../../../shared/widgets/_stylesheets/widget.spread.less");
 var { UI } = require('../../../shared/jquery/components/core');
 //var PaymentServie = require('../_services/PaymentServie');
 
 /**
- * 已经激活后的分期首页入口
+ * 支付成功页面入口
  * 入口启动代码放到UI.ready();
  *
  */
 UI.ready(function () {
-  /*var paymentService = new PaymentServie();
-  paymentService.fetchTestData(null)
-    .then(function (result) {
-      alert("success!");
-    })
-    .fail(function (err) {
-      alert("failed!");
-    });*/
+
 });
