9d3b26631f60c520559b7700958e5687c77f3398
叶俊 <yejun@yejundeMacBook-Air.local>
2015-11-13 17:24:23 +0800

    activating password valid

--- projects/activating/activating_setpassword/index.js
+++ projects/activating/activating_setpassword/index.js
@@ -55,8 +55,49 @@ var validateOptions = $.extend({}, validatorLib.DEFAULTS, {
 
 jQuery.validator.addMethod('isBaitiaoPwd', function(value, element) {
   var length = value.length;
-  var BaitiaoPwd = /^[A-Za-z0-9]+$/;   //    /[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/;  //    /^\d+$/;
-  return (length ==6 && BaitiaoPwd.exec(value)) ? true : false;
+  var BaitiaoPwd =  /^\d{6}$/; 
+  if(!BaitiaoPwd.exec(value)){
+    return false;
+  }
+    
+    if(length!=6){
+      return false;
+
+    }
+      var pwd = new Array();
+      var tempValue = value + "";
+      pwd[0]=parseInt(tempValue.substring(0,1));
+      pwd[1]=parseInt(tempValue.substring(1,2));
+      pwd[2]=parseInt(tempValue.substring(2,3));
+      pwd[3]=parseInt(tempValue.substring(3,4));
+      pwd[4]=parseInt(tempValue.substring(4,5));
+      pwd[5]=parseInt(tempValue.substring(5,6));
+
+      if (pwd[0]==pwd[1]-1
+        && pwd[1]==pwd[2]-1
+        && pwd[2]==pwd[3]-1
+        && pwd[3]==pwd[4]-1
+        && pwd[4]==pwd[5]-1) {
+        return false;
+      };
+
+      if (pwd[0]==pwd[1]+1
+        && pwd[1]==pwd[2]+1
+        && pwd[2]==pwd[3]+1
+        && pwd[3]==pwd[4]+1
+        && pwd[4]==pwd[5]+1) {
+        return false;
+      };
+
+      if (pwd[0]==pwd[1]
+        && pwd[1]==pwd[2]
+        && pwd[2]==pwd[3]
+        && pwd[3]==pwd[4]
+        && pwd[4]==pwd[5]) {
+        return false;
+      };
+
+      return true ;
 }, "请填写正确白条密码");
 
 /**
